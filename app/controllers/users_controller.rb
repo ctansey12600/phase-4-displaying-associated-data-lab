2f24ec8902f72ea2546008d8ff839dae59467e50
Casey Tansey <ctansey12600@outlook.com>
2022-11-01 18:42:25 -0400

    commit tree 3831b65f2154ca1e34fbab1abd23a0334fee7240

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  def show
+    user = find_user
+    render json: user,  except: [:created_at, :updated_at], include: :items, status: :ok
+  end
+
+  private
+
+  def find_user
+    User.find(params[:id])
+  end
+
+  def render_not_found_response
+    render json: {error: "User not found"}, status: :not_found
+  end
+
 end
